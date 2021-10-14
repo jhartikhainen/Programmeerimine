6eb70535d5539e41fe071d8d4a7addb8ec0ddcf0
jhartikhainen <92164339+jhartikhainen@users.noreply.github.com>
2021-10-14 17:37:36 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,5 +1,5 @@
 function checkTemp (temperatuur){
-if (temperatuur <= 20) return "-1"
-else if (temperatuur <= 40) return "0"
-else if (temperatuur <= 60) return "1"
+if (temperatuur <= 20) return -1
+else if (temperatuur <= 40) return 0
+else if (temperatuur <= 60) return 1
 }
